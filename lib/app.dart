e6711b49dcd714fb2dc3035c4f830552e41a9659
Babar Arif <babararifmasood98@gmail.com>
2026-04-27 23:34:24 +0400

    Splash Reverse

--- lib/app.dart
+++ lib/app.dart
@@ -1,29 +1,29 @@
 import 'package:b_store_app/utils/theme/theme.dart';
 import 'package:flutter/material.dart';
-import 'splashscreen.dart';
-
-// class App extends StatelessWidget {
-//   const App({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       themeMode: ThemeMode.system,
-//       theme: BAppTheme.lightTheme,
-//       darkTheme: BAppTheme.darkTheme,
-//     );
-//   }
-// }
+// import 'splashscreen.dart';
 
 class App extends StatelessWidget {
+  const App({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: splash(),
       themeMode: ThemeMode.system,
       theme: BAppTheme.lightTheme,
       darkTheme: BAppTheme.darkTheme,
     );
   }
 }
+
+// class App extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: splash(),
+//       themeMode: ThemeMode.system,
+//       theme: BAppTheme.lightTheme,
+//       darkTheme: BAppTheme.darkTheme,
+//     );
+//   }
+// }
