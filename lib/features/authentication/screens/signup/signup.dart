ef5fe94f0e37c6e4a0d92d4bd39981b5162d5545
Babar Arif <babararifmasood98@gmail.com>
2026-05-06 22:47:20 +0400

    SignUp UI Start

---
+++ lib/features/authentication/screens/signup/signup.dart
@@ -0,0 +1,19 @@
+import 'package:b_store_app/utils/constants/sizes.dart';
+import 'package:flutter/material.dart';
+
+class SignupScreen extends StatelessWidget {
+  const SignupScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(BSizes.defaultSpace),
+          child: Column(),
+        ),
+      ),
+    );
+  }
+}
